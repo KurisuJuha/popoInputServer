69d4b28694cca0960febd4058128475397f1cc54
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 22:02:08 +0900

    playerCountを追加

--- popoInputServer/Server.cs
+++ popoInputServer/Server.cs
@@ -6,6 +6,7 @@ namespace JuhaKurisu.PopoTools.InputSystem.Server;
 public class Server
 {
     public ReadOnlyDictionary<Guid, Client> clients => clientDictionary.AsReadOnly();
+    public readonly int playerCount;
 
     private readonly Dictionary<Guid, Client> clientDictionary;
 
