eeb817f4a1073a7c5ef19e2dd024fe1cd9b7726f
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 22:02:28 +0900

    UpdateInputを追加

--- popoInputServer/Server.cs
+++ popoInputServer/Server.cs
@@ -17,4 +17,23 @@ public class Server
         clientDictionary = clients;
     }
 
+    public void UpdateInput(byte[] bytes)
+    {
+        DataReader reader = new DataReader(bytes);
+
+        // clientIDを読み込む
+        Guid clientID = reader.ReadGuid();
+        // secretIDを読み込む
+        Guid secretID = reader.ReadGuid();
+        // input内容を読み込む
+        byte[] inputBytes = reader.ReadBytes();
+
+        // secretIDが違う場合はエラーとする
+        if (!clients.TryGetValue(clientID, out Client? client)
+            || client is null
+            || client.secretID != secretID) return;
+
+        // 新しいclientをセット
+        clientDictionary[clientID] = new Client(clientID, secretID, inputBytes);
+    }
 }
