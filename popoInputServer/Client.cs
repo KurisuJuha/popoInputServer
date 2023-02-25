442056dbf96bef1701475a4afe42b8da2cd66e83
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 21:10:36 +0900

    Clientのコンストラクタ定義

--- popoInputServer/Client.cs
+++ popoInputServer/Client.cs
@@ -7,4 +7,11 @@ public class Client
     public readonly Guid clientID;
     public readonly Guid secretID;
     public ReadOnlyCollection<byte> input;
+
+    public Client(Guid clientID, Guid secretID, byte[] bytes)
+    {
+        this.clientID = clientID;
+        this.secretID = secretID;
+        input = new(bytes);
+    }
 }
