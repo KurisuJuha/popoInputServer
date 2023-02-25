eceb5174af93723f3c74002f82e17a578ba638c4
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 22:02:00 +0900

    clientsを定義

--- popoInputServer/Server.cs
+++ popoInputServer/Server.cs
@@ -1,6 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using JuhaKurisu.PopoTools.ByteSerializer;
+
 namespace JuhaKurisu.PopoTools.InputSystem.Server;
 
 public class Server
 {
+    public ReadOnlyDictionary<Guid, Client> clients => clientDictionary.AsReadOnly();
+
+    private readonly Dictionary<Guid, Client> clientDictionary;
 
 }
